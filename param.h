f6f96a1bef77b07a2c968300d0358c9a45afd8ce
itspariya <149511635+itspariya@users.noreply.github.com>
2023-10-31 14:06:04 -0400

    Add files via upload

---
+++ param.h
@@ -0,0 +1,16 @@
+#define NPROC        64  // maximum number of processes
+#define KSTACKSIZE 4096  // size of per-process kernel stack
+#define NCPU          8  // maximum number of CPUs
+#define NOFILE       16  // open files per process
+#define NFILE       100  // open files per system
+#define NINODE       50  // maximum number of active i-nodes
+#define NDEV         10  // maximum major device number
+#define ROOTDEV       1  // device number of file system root disk
+#define MAXARG       32  // max exec arguments
+#define MAXOPBLOCKS  10  // max # of blocks any FS op writes
+#define LOGSIZE      (MAXOPBLOCKS*3)  // max data blocks in on-disk log
+#define NBUF         (MAXOPBLOCKS*3)  // size of disk block cache
+#define FSSIZE       1000  // size of file system in blocks
+#define MAX_STRING_LENGTH	1024   // max string lenght for kuniq
+#define SCHEDULING_POLICY_FCFS 1
+#define SCHEDULING_POLICY_PRIORITY 2
